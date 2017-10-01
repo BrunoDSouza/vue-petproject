b77ad5d626805abe5edc823e380da2a7f6e7c488
Bruno D. Souza <brunosouzasantos07@gmail.com>
2017-10-01 17:45:30 -0300

    Add protection to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from '@/router/routes'
-import store from '@/vuex'
+import beforeEach from '@/router/beforeEach'
 
 Vue.use(Router)
 
 const router = new Router({ routes, linkActiveClass: 'active' })
 
-router.beforeEach((to, from, next) => {
-  if (to.path.indexOf('/auth') === -1) {
-    if (store.getters.isLogged) next()
-    else next('/auth')
-
-    return
-  }
-
-  next()
-})
+router.beforeEach(beforeEach)
 
 export default router
